68b8d65767a9b8e361b9f82501e48e2b033f5679
Gerson-Gomez <ger.gomezz56@gmail.com>
2023-11-21 22:39:55 -0600

    Update Footer.js

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,1 +1,25 @@
+// Footer.js
 
+import React from 'react';
+
+const Footer = () => {
+  const footerStyles = {
+    backgroundColor: '#E9AE05', // Cambia este color al que desees
+    padding: '3px',
+    position: 'fixed',
+    bottom: 0,
+    width: '100%',
+    textAlign: 'center',
+    color: '#000000', // Cambia este color al que desees para el texto
+  };
+
+  return (
+    <footer className="bg-body-tertiary text-center text-lg-start" style={footerStyles}>
+      <div className="text-center p-3" style={{ color: '#000000' }}>
+        © {new Date().getFullYear()} Gerson Daniel Gómez Martínez.
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
